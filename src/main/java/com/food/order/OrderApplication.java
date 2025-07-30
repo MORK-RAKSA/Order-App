9aa284086cb7cfe9151f96d26d11d1e628d9b5f5
MORK-RAKSA <mork.raksa.1221@rupp.edu.kh>
2025-07-30 10:07:46 +0700

    Completed

--- src/main/java/com/food/order/OrderApplication.java
+++ src/main/java/com/food/order/OrderApplication.java
@@ -3,11 +3,14 @@ package com.food.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class OrderApplication {
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderApplication.class, args);
-	}
-
+        SpringApplication.run(OrderApplication.class, args);
+    }
 }
