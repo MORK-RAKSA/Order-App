1c974c883208ca630fe8b6b10a2f614b4a5db02f
morkraksa <141297181+MORK-RAKSA@users.noreply.github.com>
2025-08-01 13:35:21 +0700

    Update OrderApplication.java

--- src/main/java/com/food/order/OrderApplication.java
+++ src/main/java/com/food/order/OrderApplication.java
@@ -3,14 +3,9 @@ package com.food.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class OrderApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
         SpringApplication.run(OrderApplication.class, args);
     }
 }
