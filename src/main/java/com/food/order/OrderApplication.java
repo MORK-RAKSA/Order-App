939cd7f83a5630292f77e43dc5210936e5ccfb2a
MORK-RAKSA <mork.raksa.1221@rupp.edu.kh>
2025-08-28 14:29:12 +0700

    Fix auto send message, lmao

--- src/main/java/com/food/order/OrderApplication.java
+++ src/main/java/com/food/order/OrderApplication.java
@@ -1,6 +1,6 @@
 package com.food.order;
 
- import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,8 +9,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class OrderApplication {
     public static void main(String[] args) {
-         Dotenv dotenv = Dotenv.load();
-         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+//         Dotenv dotenv = Dotenv.load();
+//         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(OrderApplication.class, args);
     }
 }
